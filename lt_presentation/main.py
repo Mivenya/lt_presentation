5addeb8bb4d6b9c7ce479c6e87d5086c7d63486a
Mivenya <amanda.d.dasilva@gmail.com>
2024-08-15 13:50:46 -0700

    updating new repo to add main.py, getting project setup

---
+++ lt_presentation/main.py
@@ -0,0 +1,13 @@
+# Main File to run project. Aggregate data from Kaggle ML and DS Surveys from 2017 - 2022 to analyze trends for lightning talk presentation purposes
+
+# The purpose of the lightning talk is to show trends of growth in ML and DS.
+# Based on finding possibly highlight not just number of people but gender/location etc?
+
+#Begin with importing Libraries
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Next we will be pulling data from multiple datasets but want to narrow down what info we want.  
+# I know showing number of responses, gender, locations, employed status could all be useful for showing trends
+
